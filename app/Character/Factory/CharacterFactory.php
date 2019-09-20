33bf987587688decfff7fefa9be7d831f1952bc6
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-09-20 16:56:52 +0800

    Added CharacterFactory

--- app/Character/Factory/CharacterFactory.php
+++ app/Character/Factory/CharacterFactory.php
@@ -2,90 +2,24 @@
 
 namespace Noblesse\Character\Factory;
 
-require_once $_SERVER['DOCUMENT_ROOT'] .'vendor/autoload.php';
+require_once __DIR__.'../../../../vendor/autoload.php';
 
-use Noblesse\Character\Character;
-use Noblesse\Character\MainCharacter;
+use Noblesse\Character\Factory\CharacterSetting as CharSetting;
 
 class CharacterFactory
 {
     /**
-     * Frankenstein's setting
+     * Returns the creation of character
      *
-     * @return MainCharacter
+     * @param string $charType
+     * @return Character|MainCharacter
      */
-    public static function superModifiedHuman(): MainCharacter
+    public static function makeCharacter(string $charType)
     {
-        $frank          = new MainCharacter('Frankenstein', 'Super Modified Human', 'Dark Spear');
-        $frank->damage  = [30, 50];
-    
-        return $frank;
-    }
-    
-    /**
-     * M-21's setting
-     *
-     * @return MainCharacter
-     */
-    public static function simpleModifiedHuman(): MainCharacter
-    {
-        $m21            = new MainCharacter('M-21', 'Simple Mofidied Human', 'Gun');
-        $m21->damage    = [25, 30];
-
-        return $m21;
-    }
-
-    /**
-     * Muzaka's setting
-     *
-     * @return MainCharacter
-     */
-    public static function werewolf(): MainCharacter
-    {
-        $muzaka         = new MainCharacter('Muzaka', 'Werewolf', 'Strong Punch');
-        $muzaka->damage = [25, 45];
-
-        return $muzaka;
-    }
-
-    /**
-     * Han Shinwoo's setting
-     *
-     * @return MainCharacter
-     */
-    public static function human(): MainCharacter
-    {
-        $han            = new MainCharacter('Han Shinwoo', 'Human', 'Karate');
-        $han->damage    = [40, 45];
-
-        return $han;
-    }
-
-    /**
-     * Enemy vampire's setting
-     *
-     * @return Character
-     */
-    public static function enemyVampire(): Character
-    {
-        $vampire         = new Character('Nameless', 'Vampire', 'Claws');
-        $vampire->damage = [1, 10];
-        $vampire->health = rand(40, 50);
-
-        return $vampire;
-    }
-
-    /**
-     * The noblesse will turn into a boss if the given items are missing.
-     *
-     * @return Character
-     */
-    public static function enemyBoss(): Character
-    {
-        $noblesse           = new Character('Raizel', 'Vampire Noble', 'Blood Magic');
-        $noblesse->health   = 150;
-        $noblesse->damage   = [30, 50];
-
-        return $noblesse;
+        if (method_exists(CharSetting::class, $charType)) {
+            return CharSetting::$charType();
+        } else {
+            return NULL;
+        }
     }
 }
