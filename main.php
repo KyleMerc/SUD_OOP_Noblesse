f3ca31ac7a8560427d4e0c135b1fefc2ef5d4420
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-09-20 16:56:52 +0800

    Added CharacterFactory

--- main.php
+++ main.php
@@ -1,3 +1,33 @@
 <?php
 
-var_dump(in_array('key', ['key', 'now']));
+// class First
+// {
+//     public function firstMethod(): void
+//     {
+//         echo "Hello First World";
+//     }
+// }
+
+// class Second
+// {
+//     public static function secondMethod(?callable $sample): void
+//     {
+//         call_user_func([First::class, 'firstMethod']);
+//     }
+// }
+
+// Second::secondMethod(NULL);
+function callFunc1(Closure $closure) {
+    $closure(5);
+}
+
+function callFunc2(Callable $callback) {
+    $callback();
+}
+
+function xy() {
+    echo 'Hello, World!';
+}
+
+callFunc1(function ($x) { return $x * $x; }); // Catchable fatal error: Argument 1 passed to callFunc1() must be an instance of Closure, string given
+callFunc2("xy");
