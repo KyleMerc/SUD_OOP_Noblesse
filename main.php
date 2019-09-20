3250d59bdbadadea28e8d8ad0ee680f8f007a69a
Kyle Mercado <kyle.mercado@fullspeedtechnologies.com>
2019-09-20 17:57:02 +0800

    Completed character factory

--- main.php
+++ main.php
@@ -1,33 +1,7 @@
 <?php
 
-// class First
-// {
-//     public function firstMethod(): void
-//     {
-//         echo "Hello First World";
-//     }
-// }
+$case1 = 'enemssyVampire';
+$case2 = 'enemyBoss';
 
-// class Second
-// {
-//     public static function secondMethod(?callable $sample): void
-//     {
-//         call_user_func([First::class, 'firstMethod']);
-//     }
-// }
-
-// Second::secondMethod(NULL);
-function callFunc1(Closure $closure) {
-    $closure(5);
-}
-
-function callFunc2(Callable $callback) {
-    $callback();
-}
-
-function xy() {
-    echo 'Hello, World!';
-}
-
-callFunc1(function ($x) { return $x * $x; }); // Catchable fatal error: Argument 1 passed to callFunc1() must be an instance of Closure, string given
-callFunc2("xy");
+$res = strncmp($case1, $case2, 5);
+var_dump($res);
